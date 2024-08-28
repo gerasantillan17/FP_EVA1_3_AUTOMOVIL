b0a3c7566a056abcf3a7a94e5b5131e82dffdd4e
gerasantillan17 <gerapumas122@gmail.com>
2024-08-28 16:10:02 -0600

    Add files via upload

---
+++ Automovil.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package automovil;
+
+/**
+ *
+ * @author gerar
+ */
+public class Automovil {
+
+    /**
+     * @param args the command line arguments
+     */        
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String marca;
+        String modelo;
+        int year;
+        double precio;
+        
+        marca = "Ford";
+        modelo = "Mustang";
+        year = 2022;
+        precio = 999000.1;
+        System.out.println(marca);
+        System.out.println(modelo);
+        System.out.println(year);
+        System.out.println(precio);
+    }
+    
+}
